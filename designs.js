a0fc948aabcb5dcf1264658fece2d52fe3c75cd5
jeremief <quasimobeau@yahoo.fr>
2017-09-26 08:08:27 +1000

    Feat: add jQuery link and start experimenting with events

--- designs.js
+++ designs.js
@@ -7,4 +7,20 @@ function makeGrid() {
 
 // Your code goes here!
 
+//Capture values of grid
+const height = $('#input_height').val();
+const width = $('#input_width').val();
+const color = $('#colorPicker').val();
+
+console.log('height = '+ height);
+console.log('width = ' + width);
+console.log('color = '+ color);
+console.log("hi");
+
 }
+
+$("#submission").on('click', makeGrid());
+
+// makeGrid();
+
+
