f72c3ab9e4bfb71b54d4ec3cf96eaee9adc55507
jeremief <quasimobeau@yahoo.fr>
2017-09-26 18:21:49 +1000

    Feat: complete makeGrid function

--- designs.js
+++ designs.js
@@ -8,20 +8,39 @@ jQuery(document).ready(function(){
 
 function makeGrid() {
 
-// Your code goes here!
-
 //Capture values of grid
 const height = $('#input_height').val();
 const width = $('#input_width').val();
-const color = $('#colorPicker').val();
 
-alert('height = '+ height);
-alert('width = ' + width);
-alert('color = '+ color);
+
+// Building grid html
+$('#pixel_canvas').children().remove()
+
+let grid_html = "";
+
+for (let i = 1; i <= height; i++) {
+    grid_html += "<tr>";
+    let j = 1;
+    while (j <= width) {
+        grid_html += "<td></td>";
+        j++;
+    }
+    grid_html += "</tr>";
+}
+
+$('#pixel_canvas').prepend(grid_html);
+
+
+// Prevent the refresh of the page
+event.preventDefault();
+
 
 }
 
 $(":submit").click(makeGrid);
 
+
 });
+
+
 
